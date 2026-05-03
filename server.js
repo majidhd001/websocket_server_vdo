f1b35dee1c2f7e010b86de3a9bb20181241c3541
majidhd001 <87666707+majidhd001@users.noreply.github.com>
2026-05-03 12:41:30 +0330

    change server.js for local

--- server.js
+++ server.js
@@ -1,45 +1,27 @@
-//
-// Copyright (c) 2021 Steve Seguin. All Rights Reserved.
-// Use of this source code is governed by the AGPLv3 open-source license.
-// Use at your own risk, as it may contain bugs or security vulnerabilities.
-//
-///// INSTALLATION
-// sudo apt-get update
-// sudo apt-get upgrade
-// sudo apt-get install nodejs -y
-// sudo apt-get install npm -y
-// sudo npm install express
-// sudo npm install ws
-// sudo add-apt-repository ppa:certbot/certbot  
-// sudo apt-get install certbot -y
-// sudo certbot certonly // register your domain
-// sudo nodejs server.js // port 443 needs to be open. THIS STARTS THE SERVER
-//
-//// Finally, if using this with a ninja deploy, update index.html of the ninja installation as needed, such as with:
-//  session.wss = "wss://wss.contribute.cam:443";
-//  session.customWSS = true;  #  Please refer to the vdo.ninja instructions for exact details on settings; this is just a demo.
-/////////////////////////
-
 "use strict";
-var fs = require("fs");
-var https = require("https");
+
 var express = require("express");
-var app = express();
+var http = require("http");
 var WebSocket = require("ws");
 
-const key = fs.readFileSync("/etc/letsencrypt/live/wss.contribute.cam/privkey.pem"); /// UPDATE THIS PATH
-const cert = fs.readFileSync("/etc/letsencrypt/live/wss.contribute.cam/fullchain.pem"); /// UPDATE THIS PATH
+var app = express();
 
-var server = https.createServer({key,cert}, app);
+// اگر خواستی فایل‌ها رو هم سرو کنی
+app.use(express.static(__dirname));
+
+var server = http.createServer(app);
 var websocketServer = new WebSocket.Server({ server });
 
-websocketServer.on('connection', (webSocketClient) => {
-    webSocketClient.on('message', (message) => {
-            websocketServer.clients.forEach( client => {
-                    if (webSocketClient!=client){
-                        client.send(message.toString());
-                    }
-            });
+websocketServer.on("connection", (ws) => {
+    ws.on("message", (message) => {
+        websocketServer.clients.forEach((client) => {
+            if (client.readyState === WebSocket.OPEN && client !== ws) {
+                client.send(message.toString());
+            }
+        });
     });
 });
-server.listen(443, () => {console.log(`Server started on port 443`) });
+
+server.listen(8080, "0.0.0.0", () => {
+    console.log("LAN WebSocket server running on port 8080");
+});
